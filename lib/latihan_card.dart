02090e46cd142986436ea6d8c6372e155f62ff5c
Muhamad Ayesha Aulia <shaxyyy.03@gmail.com>
2025-11-21 20:14:41 +0700

    membuat file latihan_card

---
+++ lib/latihan_card.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LatihanCard extends StatelessWidget {
+  const LatihanCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:week_8_card/latihan_card.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Latian Card',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: LatihanCard(),
+    );
+  }
+}
