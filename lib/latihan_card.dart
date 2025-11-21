550b1c2da4acad7a493aa6d7841c92189fb034aa
Muhamad Ayesha Aulia <shaxyyy.03@gmail.com>
2025-11-21 20:20:51 +0700

    membuat text untuk dashboard

--- lib/latihan_card.dart
+++ lib/latihan_card.dart
@@ -5,6 +5,12 @@ class MycardNight extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Dahboard'),
+        actions: const [],
+      ),
+      
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
       title: 'Latian Card',
       theme: ThemeData(primarySwatch: Colors.blue),
       home: MycardNight(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
